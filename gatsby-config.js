accd3644956b51a9af5625156ece0ede3f8ef7d4
swatts89 <srlwatts8@gmail.com>
2021-06-03 23:12:40 +0100

    Add image and plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,8 @@ module.exports = {
   siteMetadata: {
     title: "srlwatts gatbsy site",
   },
-  plugins: ["gatsby-plugin-gatsby-cloud"],
+  plugins: [
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp"],
 };
